9c86a5d0785473856298797d18adfeebd50e75fc
EvgeniyaJo <evgeniyajo92@gmail.com>
2024-09-24 19:58:21 +0500

    задания по js 1-10

---
+++ Homework1/script.js
@@ -0,0 +1,67 @@
+//1 задание
+let a = Number(10);
+alert(a);
+a = Number(20);
+alert(a);
+
+//2 задание
+const releaseYear = (`2007 - Год выпуска 1 IPhone`);
+alert(releaseYear);
+
+//3 задание
+const creatorName = (`Брендан Эйх - имя создателя JS`);
+alert(creatorName);
+
+//4 задание
+let b = Number(10);
+let c = Number(2);
+let sum = b + c;
+let dif = b - c; 
+let prod = b * c;
+let div = b / c;
+alert(sum);
+alert(dif);
+alert(prod);
+alert(div);
+
+//5 задание
+let d = Number(2);
+let result = d ** 5;
+alert(result);
+
+//6 задание
+let e = Number(9);
+let f = Number(2);
+let rem = e % f;
+alert(rem);
+
+//7 задание
+let num = 1;
+alert(num += 5);
+alert(num -= 3);
+alert(num *= 7);
+alert(num /= 3);
+alert(++num);
+alert(--num);
+
+//8 задание
+let age = prompt(`Сколько Вам лет?`);
+alert(age);
+
+//9 задание
+const user = {
+    name: String(`Stiv`),
+    age: Number(`32`),
+    isAdmin: Boolean (`true`)
+}
+let info = prompt(`Что Вы хотите узнать о пользователе? Например, имя?`, `name`);
+alert(user[info]);
+info = prompt(`Что Вы хотите узнать о пользователе что-то еще? Например, возраст?`, `age`);
+alert(user[info]);
+info = prompt(`Вас интересует еще что-то? Например, является ли он администратором?`, `isAdmin`);
+alert(user[info]);
+
+//10 задание
+let name = prompt (String(`Как Ваше имя?`));
+let hello = (`Привет, ${name}!`);
+alert(hello);
