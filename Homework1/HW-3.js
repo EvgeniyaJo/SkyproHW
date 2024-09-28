ac0e1479b6a58626d6f03bd27dd68af7c5c72353
EvgeniyaJo <evgeniyajo92@gmail.com>
2024-09-29 00:30:24 +0500

    1 часть заданий по js сделана

---
+++ Homework1/HW-3.js
@@ -0,0 +1,64 @@
+/*//1 задание
+let password = String `122345qwerty`;
+let password2 = String (prompt (`Введите пароль`));
+alert (password===password2 ? `Пароль введен верно` : `Пароль введен неверно`);
+
+//2 задание
+let c = Number(prompt(`введите число от 1 до 10`));
+alert (c>=0 && c<=10 ? `Верно` : `Неверно`);
+
+//3 задание
+let d = Number(prompt(`Введите первое число?`));
+let e = Number(prompt(`Введите второе число?`));
+alert (d>100 || e>100 ? `Верно` : `Неверно`);
+
+//4 задание
+let a = '2';
+let b = '3';
+alert(Number (a) + Number (b));
+
+//5 задание
+var monthNumber = prompt(`Введите номер месяца в году для определния сезона?`);
+switch (monthNumber) {
+    case '1':
+        alert('Зима');
+        break;
+    case '2':
+        alert('Зима');
+        break;
+    case '3':
+        alert('Весна');
+        break;
+    case '4':
+        alert('Весна');
+        break;
+    case '5':
+        alert('Весна');
+        break;
+    case '6':
+        alert('Лето');
+        break;
+    case '7':
+        alert('Лето');
+        break;
+    case '8':
+        alert('Лето');
+        break;
+    case '9':
+        alert('Осень');
+        break;
+    case '10':
+        alert('Осень');
+        break;
+    case '11':
+        alert('Осень');
+        break;
+    case '12':
+        alert('Зима');
+        break;
+
+    default:
+        alert('В году всего 12 месяцев');
+        break;
+}
+*/
